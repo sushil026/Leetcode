f7b7fbcfd558bbc18cbc3f448586f1800b254ee4
sushil026 <68173862+sushil026@users.noreply.github.com>
2022-07-24 23:59:27 +0530

    Update 155-min-stack.cpp

--- 155-min-stack/155-min-stack.cpp
+++ 155-min-stack/155-min-stack.cpp
@@ -34,3 +34,33 @@ public:
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
  */
+class MinStack {
+    stack<int> stc;
+    stack<int> mins;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        stc.push(val);
+        if( mins.empty() || val<=mins.top()){
+            mins.push(val);
+        }
+    }
+    
+    void pop() {
+        if( stc.top() == mins.top()){
+            mins.pop();
+        }
+        stc.pop();
+    }
+    
+    int top() {
+        return stc.top();
+    }
+    
+    int getMin() {
+        return mins.top();
+    }
+};
